689e7e92b61f251e2b4d0219a734e68161514495
Purshi-17 <purshipurshu254@gmail.com>
2024-08-31 12:54:06 +0530

    Create characters of string.c

---
+++ characters of string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{int n,i;
+  char a[10];
+  printf("enter the numebr\n");
+  scanf("%d",&n);
+  printf("enter the string\n");
+  scanf("%s",a);
+  for(i=0;i<n;i++)
+    {     printf("%c\n",a[i]);}}
