43a0ff0fc86f46d40e010f754bb2bd36a5ea6ceb
Purshi-17 <purshipurshu254@gmail.com>
2024-08-31 13:12:32 +0530

    Update structure program.c

--- structure program.c
+++ structure program.c
@@ -8,6 +8,6 @@ void main()
 {  struct book hp;
   hp.num=1;
   hp.price=100.0;
-  strcpy(hp.name,"meghavarshini");
+  strcpy(hp.name,"Purshipurshu");
   printf("book numberis:%d\n",hp.num);
   printf("book price=%f\n",hp.price); printf("book name=%s\n",hp.name);}
