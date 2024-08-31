b83e3f829a61c4481adc87ff0ba84871f2a93104
Purshi-17 <purshipurshu254@gmail.com>
2024-08-31 13:38:52 +0530

    Create create a node.c

---
+++ create a node.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct Node
+{int data;
+struct Node *next;};
+struct Node *createNode(int data)
+{  struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
+  newNode->data=data;
+  newNode->next=NULL;
+  return newNode;}
